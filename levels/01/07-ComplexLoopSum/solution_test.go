0d4f6b0209ff3caf8b29d7e23af97317a4748994
kirinnee <kirinnee97@gmail.com>
2025-01-06 01:58:52 +0800

    feat: 01 07

---
+++ levels/01/07-ComplexLoopSum/solution_test.go
@@ -0,0 +1,27 @@
+package fizzbuzz0101
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type KeepWriter struct {
+	result []interface{}
+}
+
+func (k *KeepWriter) Println(a ...interface{}) {
+	for _, v := range a {
+		k.result = append(k.result, v)
+	}
+	fmt.Println(a...)
+	return
+}
+
+func Test_Solution(t *testing.T) {
+
+	fake := new(KeepWriter)
+	Solution(fake)
+	assert.Equal(t, []interface{}{6346}, fake.result)
+
+}
---
+++ levels/01/07-ComplexLoopSum/solution_test.py
@@ -0,0 +1,23 @@
+from solution import solution
+import unittest
+
+
+def print_gen():
+    result = []
+    def p(a):
+        result.append(a)
+        print(a)
+    return [p, result]
+
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_solution(self):
+        [fake, result] = print_gen()
+        solution(fake)
+        expected = [6346]
+        self.assertEqual(result, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
