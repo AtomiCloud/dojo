6474cfc24bc822f2d698ad9f4115c3ac7ea260f3
kirinnee <kirinnee97@gmail.com>
2025-01-05 23:17:19 +0800

    feat: initial commit

---
+++ nix/env.nix
@@ -0,0 +1,39 @@
+{ pkgs, packages }:
+with packages;
+{
+  system = [
+    coreutils
+    findutils
+    gnugrep
+    gnused
+    bash
+    jq
+    yq
+  ];
+
+  dev = [
+    pls
+    git
+  ];
+
+  main = [
+    infisical
+    bun
+    go
+    python
+    dotnet
+    richgo
+  ];
+
+  lint = [
+    # core
+    treefmt
+    gitlint
+    shellcheck
+    sg
+  ];
+
+  releaser = [
+    sg
+  ];
+}
---
+++ nix/packages.nix
@@ -0,0 +1,47 @@
+{ pkgs, pkgs-2411, atomi }:
+let
+
+  all = {
+    atomipkgs = (
+      with atomi;
+      {
+        inherit
+          sg
+          pls;
+      }
+    );
+    nix-unstable = (
+      with pkgs;
+      { }
+    );
+    nix-2411 = (
+      with pkgs-2411;
+      {
+        yq = yq-go;
+        python = python314;
+        dotnet = dotnet-sdk_9;
+        inherit
+          coreutils
+          findutils
+          gnugrep
+          gnused
+          jq
+          bash
+          git
+          infisical
+          treefmt
+          gitlint
+          shellcheck
+          bun
+          go
+          richgo
+
+          ;
+      }
+    );
+  };
+in
+with all;
+nix-2411 //
+nix-unstable //
+atomipkgs
