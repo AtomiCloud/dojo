fb90c059ecc3b012e4db8842f0c47afe8a67fb0c
kirinnee <kirinnee97@gmail.com>
2025-01-06 01:21:37 +0800

    feat: 01 04

--- levels/01/04-SmallSquareBigDouble/Solution.Test.cs
+++ levels/01/04-SmallSquareBigDouble/Solution.Test.cs
@@ -58,8 +58,7 @@ public class Tester
     {
         var fake = new SpyWriter();
         Solution.Start(fake);
-        List<object> actual = ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz",
-            "16", "17", "Fizz", "19", "Buzz", "Fizz", "22", "23", "Fizz", "Buzz", "26", "Fizz", "28", "29", "FizzBuzz"];
+        List<object> actual = [49,64,81,100,121,144,169,196,225,256,289,324,361,40,42,44,46,48,50,52,54,56,58,60,62,64,66,68,70];
         Assert.Equal(actual, fake.Results);
     }
 }
