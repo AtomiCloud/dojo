a7973456afdf398ae5a0d1a323e5de864eeae4ee
kirinnee <kirinnee97@gmail.com>
2025-01-06 18:34:37 +0800

    feat: 01 13

--- levels/01/13-JumpingMatrixSum/solution_test.go
+++ levels/01/13-JumpingMatrixSum/solution_test.go
@@ -22,7 +22,6 @@ func Test_Solution(t *testing.T) {
 
 	fake := new(KeepWriter)
 	Solution(fake)
-	assert.Equal(t, []interface{}{"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz", "16", "17",
-		"Fizz", "19", "Buzz", "Fizz", "22", "23", "Fizz", "Buzz", "26", "Fizz", "28", "29", "FizzBuzz"}, fake.result)
+	assert.Equal(t, []interface{}{3,6,9,12,15,18,9,18,27,36,45,54,15,30,45,60,75,90,21,42,63,84,105,126,27,54,81,108,135,162,33,66,99,132,165,198,39,78,117,156,195,234,45,90,135,180,225,270,51,102,153,204,255,306,57,114,171,228,285,342}, fake.result)
 
 }
