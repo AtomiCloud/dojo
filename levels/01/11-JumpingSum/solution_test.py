e802262cae9dce272d342ce2bdb0e258086fbd76
kirinnee <kirinnee97@gmail.com>
2025-01-06 18:19:45 +0800

    feat: 01 11

--- levels/01/11-JumpingSum/solution_test.py
+++ levels/01/11-JumpingSum/solution_test.py
@@ -15,8 +15,7 @@ class TestStringMethods(unittest.TestCase):
     def test_solution(self):
         [fake, result] = print_gen()
         solution(fake)
-        expected = ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz", "16", "17",
-                    "Fizz", "19", "Buzz", "Fizz", "22", "23", "Fizz", "Buzz", "26", "Fizz", "28", "29", "FizzBuzz"]
+        expected = [469]
         self.assertEqual(result, expected)
 
 
